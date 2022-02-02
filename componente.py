a837c4635f3627c0c1c5a7e16b3affdbc3d9c14d
joscanoga <johan.cano0524@gmail.com>
2022-02-02 18:21:58 -0500

    modificaciones pequeñas

--- componente.py
+++ componente.py
@@ -13,7 +13,7 @@ class configuracion(componente):
     def __init__(self,id,parent,value,df):
         componente.__init__(self,id,parent,value)
         hijos=df[df["parent"]==self.id]
-        self.ajustes={"fondo": "blue","titulo" : "blue","fuente" : "arial","colorFuente" : "green","max-width" : "1000px","justificacion" : "center","text-align" : "center"}
+        self.ajustes={"fondo": "formulario","titulo" : "blue","fuente" : "arial","colorFuente" : "green","max-width" : "1000px","justificacion" : "center","text-align" : "center"}
         for i in hijos.index:
             x=re.findall(r"(\S.+\S)\s*:\s*(\S.+\S)",hijos["value"][i])[0]
             self.ajustes[x[0]]=x[1]
@@ -22,7 +22,7 @@ class configuracion(componente):
         elif self.ajustes["justificacion"]=="right":
             self.ajustes["justificacion"]="position: absolute;right: 5px"
         elif self.ajustes["justificacion"]=="left":
-            self.ajustes["justificacion"]="position: absolute;right: 5px"
+            self.ajustes["justificacion"]="position: absolute;left: 5px"
 
 class pregunta(componente):
     def __init__(self, id, parent, value, df,id2):
